4d60f1d9504b7f10fbb58a1766e563bd08c0d832
vivian walls <doly8328@gmail.com>
2022-06-03 21:52:25 -0500

    código con react y tailwind

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       <h1 className="text-3xl font-bold underline bg-red-600">
+      Hello world!
+        </h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
